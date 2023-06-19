9d84636aeb05ecbb8124e7d3e16107b1698354a4
ttionya <git@ttionya.com>
2023-06-19 17:26:57 +0800

    feat: add dayjs with locale

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './init/register-dayjs'
+
 const app = createApp(App)
 
 app.use(createPinia())
